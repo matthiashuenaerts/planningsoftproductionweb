90d9d88c5e3c911910e42347da77dac2a41512a3
matthiashuenaerts <58091363+matthiashuenaerts@users.noreply.github.com>
2025-10-12 14:24:42 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -79,6 +79,7 @@ const NavbarContent = ({
             </NavLink>
           </li>
           <li>
+            {currentEmployee && ['admin', 'manager', 'installation_team', 'teamleader'].includes(currentEmployee.role) && <li>
             <NavLink to={createLocalizedPath("/control-panel")} className="flex items-center p-2 rounded-lg hover:bg-sky-700 group" onClick={handleItemClick}>
               <LayoutDashboard className="w-5 h-5 text-white group-hover:text-white" />
               <span className="ml-3">{t('control_panel')}</span>
